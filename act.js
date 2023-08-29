d3083188692698745f5e55ce5301d3359dfa5c3b
Alvaro Sanchez <alvaro_sanchez13@hotmail.com>
2023-08-29 19:19:46 -0300

    Modificar boton mostrar mas

--- act.js
+++ act.js
@@ -1,11 +1,21 @@
 // Scrolling Effect
 
-$(window).on("scroll", function() {
-	if($(window).scrollTop()) {
-		  $('.cabeza nav').addClass('bg-black');
-	}
 
-	else {
-		  $('.cabeza nav').removeClass('bg-black');
+// function mostrar(){
+// 	console.log("funco");
+// 	document.getElementById("info").style.display = 'block';
+// }
+let h_btn = document.querySelector("#efecto");
+let h_text = document.querySelector("#texto");
+
+h_btn.addEventListener("click", toggleText);
+
+function toggleText(){
+	h_text.classList.toggle("show");
+
+	if(h_text.classList.contains("show")){
+		h_btn.innerHTML = "Mostrar menos";
+	} else{
+		h_btn.innerHTML = "Mostrar más";
 	}
-})
+}
